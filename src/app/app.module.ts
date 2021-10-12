5634e93e0d507bad6708916cbd35bad8369ca6c3
Veronika Hniličková <veronika.hnilickova@qest.cz>
2021-10-12 13:41:45 +0200

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageOneComponent } from './page-one/page-one.component';
 import { PageTwoComponent } from './page-two/page-two.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent, PageOneComponent, PageTwoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/page-two/page-two.component.ts
+++ src/app/page-two/page-two.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-page-two',
   templateUrl: './page-two.component.html',
   styleUrls: ['./page-two.component.css']
 })
 export class PageTwoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
